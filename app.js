cb8405c645c6a346ed81fd525ae960060afc59d3
swapnil lothe <44019530+swapnillothe@users.noreply.github.com>
2019-04-17 18:35:16 +0530

    Intialize the to do app

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    res.send('Todo App');
+});
+
+app.listen(PORT, () => {
+    console.log(`server is listening ${PORT}`);
+});
